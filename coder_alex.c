f5592f327d49870495492c91536f3fc93f1e72c8
XMabbX <marc.a.c@hotmail.com>
2016-09-30 11:54:22 +0200

    Arregla cagada alex

--- coder_alex.c
+++ coder_alex.c
@@ -1,43 +1,32 @@
 #include <stdio.h>
 
-int getNeighbour(int iteration){
+int getNewMatrix(){
 
-	int position_x, position_y; // row and colum
-	int neighbour[3][3];
+	int new_matrix[SIZE][SIZE];
 
-	position_x = iteration/(SIZE);
-	position_y = (iteration - position_x * SIZE) - 1;
+  for (int f = 0; f < SIZE; f++) {					//go over rows big matrix
+    for (int c = 0; c < SIZE; c++) {				//go over columns big matrix
 
-	for (int i = 0; i < 3; i++) {			//go over rows
-		for (int j = 0; j < 3; j++) {		//go over columns
-			if (((position_x - 1) + i) < 0 || ((position_y - 1) + j) < 0 ||
-				 	((position_x - 1) + i) >= SIZE || ((position_y - 1) + j) >= SIZE){ //Borders condition
-				neighbour[i][j] = - 1;
-			} else {
-				neighbour[i][j] = matrix[(position_x - 1) + i][(position_y - 1) + j];
-			}
-		}
-	}
+      live = 0;																//inicialize number of alive cell in the neighbour
 
-	return neighbour;
+      for (int i = 0; i < 3; i++) {						//go over rows matrix 3x3
+        for (int j = 0; j < 3; j++) {					//go over columns matrix 3x3
 
-}
-
-bool state (int neighbour){
-
-	int live = 0;
-	bool state;
-	for (int i = 0; i < 3; i++) {
-		for (int j = 0; j < 3; j++) {
-			if (neighbour[i][j] == 1) live++;
-		}
-	}
-	//RULES:
-	if (neighbour[1][1] == 1){									//live central cell
-		if (live < 2 || live > 3){state = true;}	//first and third rule
-		else {state = false;}											//second rule
-	} elseif(live == 3) {												//dead central cell & fourth rule
-		state = true;
-	}
-	return state; // true = live, false = dead
+          if (((f - 1) + i) >= 0 && ((c - 1) + j) >= 0 && ((f - 1) + i) < SIZE
+              && ((c - 1) + j) < SIZE && matrix[(f - 1) + i][(c - 1) + j] == 0){ //Borders condition
+              if (i == 1 && j == 1){}
+              else live++;
+          }
+        }
+      }
+      //RULES: (0 = alive; 1 = dead)
+      if (matrix[f][c] == 0){															//#1:live central cell
+        if (live < 2 || live > 3){new_matrix[f][c] = 1;}	//#2:first and third rule
+        else {new_matrix[f][c] = 0;}											//#3:second rule
+      } else if(matrix[f][c] == 1 && live == 3) {					//#4:dead central cell & fourth rule
+        new_matrix[f][c] = 0;
+      }
+    }
+  }
+	return new_matrix;
 }
